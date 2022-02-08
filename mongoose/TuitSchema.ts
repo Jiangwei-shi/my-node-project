ef0bdfa45f56cddf7ae2ed2871d00c8ed8354a55
jiangweishi <a1006351432@gmail.com>
2022-02-08 14:47:58 -0500

    finished the part 3 first section

---
+++ mongoose/TuitSchema.ts
@@ -0,0 +1,8 @@
+import mongoose, {Schema} from "mongoose";
+//from professor posted on piazza
+const TuitSchema = new mongoose.Schema({
+    tuit: {type: String, required: true},
+    postedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
+    postedOn: {type: Date, default: Date.now}
+}, {collection: "tuits"});
+export default TuitSchema;
